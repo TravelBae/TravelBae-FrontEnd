097ce02ae7a3801d12e4d6df2db798a3ffed6778
MUHAMMAD RIZQI RAMADHAN <rizqiramadhan2312@gmail.com>
2022-04-17 02:25:00 +0700

    edit owner homepage card + responsive

--- src/pages/Owner/HomeOwner.jsx
+++ src/pages/Owner/HomeOwner.jsx
@@ -13,115 +13,183 @@ export default function HomeOwner(props) {
         </div>
         <div className="flex-col place-content-center xl:mx-20 2xl:mx-56 lg:mx-16 md:mx-12 sm:mx-8 mt-10 mx-8">
           <div className="flex justify-center">
-            <div className="bg-blue-500 text-white rounded-3xl w-[18em] px-10 py-8 text-xl
+            <div className="bg-blue-500 text-white rounded-3xl w-[21em] px-10 py-8 text-xl
             2xl:h-60 2xl:px-10 2xl:py-14 2xl:text-4xl 2xl:w-[40em]
             xl:h-52 xl:px-10 xl:py-12 xl:text-3xl xl:w-[36em]
-            lg:h-48 lg:px-10 lg:py-10 lg:text-2xl lg:w-[32em]
+            lg:h-48 lg:px-10 lg:py-10 lg:text-2xl lg:w-[31em]
             md:h-44 md:px-10 md:py-8 md:text-xl md:w-[28em]">
               <p>Total Sales</p>
-              <p className="mt-8">Rp.521.000.000</p>
+              <p className="mt-8 font-bold">Rp.521.000.000</p>
             </div>
           </div>
-          <div className="flex mt-5 text-center justify-center space-x-10
+          <div className="flex mt-5 justify-center space-x-10
           2xl:space-x-40 xl:space-x-12 lg:space-x-8 md:space-x-12">
-            <div className="bg-white text-black rounded-2xl w-[10em] h-32
+            <div className="bg-white text-black rounded-2xl w-[12em] h-32 flex justify-start
             2xl:h-72 2xl:w-[40em]
             xl:h-60  xl:w-[32em]
             lg:h-48  lg:w-[22em]
             md:h-32  md:w-[16em]">
-              <Icon
-                icon="tabler:user"
-                className="w-8 h-8 inline-block mt-5
-                2xl:text-4xl 2xl:mt-14 2xl:w-16 2xl:h-16
-                xl:text-3xl xl:mt-12 xl:w-14 xl:h-14
-                lg:text-2xl lg:mt-10 lg:w-12 lg:h-12
-                md:text-xl md:mt-4 md:w-10 md:h-10"
-              />
-              <p className="text-md
-               2xl:text-3xl
-               xl:text-2xl
-               lg:text-xl
-               md:text-lg">Total User</p>
-              <p className="text-lg text-blue-500 mt-1
-              2xl:text-4xl 2xl:mt-5
-              xl:text-3xl xl:mt-4
-              lg:text-2xl lg:mt-3
-              md:text-xl md:mt-2">369</p>
+              <div className="flex mt-9 space-x-4 ml-7
+              2xl:space-x-18 2xl:ml-18 2xl:mt-24
+              xl:space-x-12 xl:ml-14 xl:mt-[4em]
+              lg:space-x-8 lg:ml-10 lg:mt-14
+              md:space-x-5 md:ml-7 md:mt-8">
+                <div>
+                  <div class="h-12 w-12 rounded-xl justify-center place-items-center bg-green-100 inline-flex
+                  2xl:text-4xl 2xl:w-24 2xl:h-24
+                  xl:text-3xl xl:w-20 xl:h-20
+                  lg:text-2xl lg:w-16 lg:h-16
+                  md:text-xl md:w-14 md:h-14">
+                    <Icon
+                      icon="tabler:user"
+                      color="166534"
+                      className="w-6 h-6 inline-block
+                      2xl:text-4xl 2xl:w-12 2xl:h-12
+                      xl:text-3xl xl:w-10 xl:h-10
+                      lg:text-2xl lg:w-8 lg:h-8
+                      md:text-xl md:w-6 md:h-6"
+                    />
+                  </div>
+                </div>
+                <div class="justify-start">
+                  <p className="text-md text-neutral-500
+                  2xl:text-3xl
+                  xl:text-2xl
+                  lg:text-xl
+                  md:text-lg">Total User</p>
+                  <p className="text-lg text-neutral-800 font-bold mt-1
+                  2xl:text-4xl 2xl:mt-5
+                  xl:text-3xl xl:mt-4
+                  lg:text-2xl lg:mt-3
+                  md:text-xl md:mt-2">369</p>
+                </div>
+              </div>
             </div>
-            <div className="bg-white text-black rounded-2xl w-[10em] h-32
-            2xl:h-72 2xl:w-[40em]
-            xl:h-60  xl:w-[32em]
-            lg:h-48  lg:w-[24em]
-            md:h-32  md:w-[16em]">
-              <Icon
-                icon="tabler:home-2"
-                className="w-8 h-8 inline-block mt-5
-                2xl:text-4xl 2xl:mt-14 2xl:w-16 2xl:h-16
-                xl:text-3xl xl:mt-12 xl:w-14 xl:h-14
-                lg:text-2xl lg:mt-10 lg:w-12 lg:h-12
-                md:text-xl md:mt-4 md:w-10 md:h-10"
-              />
-              <p className="text-md
-               2xl:text-3xl
-               xl:text-2xl
-               lg:text-xl
-               md:text-lg">Total Tour Place</p>
-              <p className="text-lg text-blue-500 mt-1
-              2xl:text-4xl 2xl:mt-5
-              xl:text-3xl xl:mt-4
-              lg:text-2xl lg:mt-3
-              md:text-xl md:mt-2">250</p>
-            </div>
-          </div>
-          <div className="flex mt-5 text-center justify-center space-x-10
-          2xl:space-x-40 xl:space-x-12 lg:space-x-8 md:space-x-12">
-            <div className="bg-white text-black rounded-2xl w-[10em] h-32
+            <div className="bg-white text-black rounded-2xl w-[12em] h-32 flex justify-start
             2xl:h-72 2xl:w-[40em]
             xl:h-60  xl:w-[32em]
             lg:h-48  lg:w-[22em]
             md:h-32  md:w-[16em]">
-              <Icon
-                icon="tabler:shopping-cart"
-                className="w-8 h-8 inline-block mt-5
-                2xl:text-4xl 2xl:mt-14 2xl:w-16 2xl:h-16
-                xl:text-3xl xl:mt-12 xl:w-14 xl:h-14
-                lg:text-2xl lg:mt-10 lg:w-12 lg:h-12
-                md:text-xl md:mt-4 md:w-10 md:h-10"
-              />
-              <p className="text-md
-               2xl:text-3xl
-               xl:text-2xl
-               lg:text-xl
-               md:text-lg">Unconfirmed Orders</p>
-              <p className="text-lg text-blue-500 mt-1
-              2xl:text-4xl 2xl:mt-5
-              xl:text-3xl xl:mt-4
-              lg:text-2xl lg:mt-3
-              md:text-xl md:mt-2">3</p>
+              <div className="flex mt-9 space-x-4 ml-7
+              2xl:space-x-18 2xl:ml-18 2xl:mt-24
+              xl:space-x-12 xl:ml-14 xl:mt-[4em]
+              lg:space-x-8 lg:ml-10 lg:mt-14
+              md:space-x-5 md:ml-7 md:mt-8">
+                <div>
+                  <div class="h-12 w-12 rounded-xl justify-center place-items-center bg-purple-100 inline-flex
+                  2xl:text-4xl 2xl:w-24 2xl:h-24
+                  xl:text-3xl xl:w-20 xl:h-20
+                  lg:text-2xl lg:w-16 lg:h-16
+                  md:text-xl md:w-14 md:h-14">
+                    <Icon
+                      icon="tabler:home-2"
+                      color="6b21a8"
+                      className="w-6 h-6 inline-block
+                      2xl:text-4xl 2xl:w-12 2xl:h-12
+                      xl:text-3xl xl:w-10 xl:h-10
+                      lg:text-2xl lg:w-8 lg:h-8
+                      md:text-xl md:w-6 md:h-6"
+                    />
+                  </div>
+                </div>
+                <div class="justify-start">
+                  <p className="text-md text-neutral-500
+                  2xl:text-3xl
+                  xl:text-2xl
+                  lg:text-xl
+                  md:text-lg">Total Tour Place</p>
+                  <p className="text-lg text-neutral-800 font-bold mt-1
+                  2xl:text-4xl 2xl:mt-5
+                  xl:text-3xl xl:mt-4
+                  lg:text-2xl lg:mt-3
+                  md:text-xl md:mt-2">250</p>
+                </div>
+              </div>
             </div>
-            <div className="bg-white text-black rounded-2xl w-[10em] h-32
+          </div>
+          <div className="flex mt-5 justify-center space-x-10
+          2xl:space-x-40 xl:space-x-12 lg:space-x-8 md:space-x-12">
+            <div className="bg-white text-black rounded-2xl w-[12em] h-32 flex justify-start
             2xl:h-72 2xl:w-[40em]
             xl:h-60  xl:w-[32em]
-            lg:h-48  lg:w-[24em]
+            lg:h-48  lg:w-[22em]
             md:h-32  md:w-[16em]">
-              <Icon
-                icon="tabler:calendar-event"
-                className="w-8 h-8 inline-block mt-5
-                2xl:text-4xl 2xl:mt-14 2xl:w-16 2xl:h-16
-                xl:text-3xl xl:mt-12 xl:w-14 xl:h-14
-                lg:text-2xl lg:mt-10 lg:w-12 lg:h-12
-                md:text-xl md:mt-4 md:w-10 md:h-10"
-              />
-              <p className="text-md
-               2xl:text-3xl
-               xl:text-2xl
-               lg:text-xl
-               md:text-lg">Total Event</p>
-              <p className="text-lg text-blue-500 mt-1
-              2xl:text-4xl 2xl:mt-5
-              xl:text-3xl xl:mt-4
-              lg:text-2xl lg:mt-3
-              md:text-xl md:mt-2">20</p>
+              <div className="flex mt-6 space-x-4 ml-5
+              2xl:space-x-18 2xl:ml-18 2xl:mt-24
+              xl:space-x-12 xl:ml-14 xl:mt-[4em]
+              lg:space-x-8 lg:ml-10 lg:mt-14
+              md:space-x-5 md:ml-7 md:mt-5">
+                <div>
+                  <div class="h-12 w-12 rounded-xl justify-center place-items-center bg-sky-100 inline-flex
+                  2xl:text-4xl 2xl:w-24 2xl:h-24
+                  xl:text-3xl xl:w-20 xl:h-20
+                  lg:text-2xl lg:w-16 lg:h-16
+                  md:text-xl md:w-14 md:h-14">
+                    <Icon
+                      icon="tabler:shopping-cart"
+                      color="075985"
+                      className="w-6 h-6 inline-block
+                      2xl:text-4xl 2xl:w-12 2xl:h-12
+                      xl:text-3xl xl:w-10 xl:h-10
+                      lg:text-2xl lg:w-8 lg:h-8
+                      md:text-xl md:w-6 md:h-6"
+                    />
+                  </div>
+                </div>
+                <div class="justify-start">
+                  <p className="text-md text-neutral-500
+                  2xl:text-3xl
+                  xl:text-2xl
+                  lg:text-xl
+                  md:text-lg">Unconfirmed Orders</p>
+                  <p className="text-lg text-neutral-800 font-bold mt-1
+                  2xl:text-4xl 2xl:mt-5
+                  xl:text-3xl xl:mt-4
+                  lg:text-2xl lg:mt-3
+                  md:text-xl md:mt-2">3</p>
+                </div>
+              </div>
+            </div>
+            <div className="bg-white text-black rounded-2xl w-[12em] h-32 flex justify-start
+            2xl:h-72 2xl:w-[40em]
+            xl:h-60  xl:w-[32em]
+            lg:h-48  lg:w-[22em]
+            md:h-32  md:w-[16em]">
+              <div className="flex mt-9 space-x-4 ml-7
+              2xl:space-x-18 2xl:ml-18 2xl:mt-24
+              xl:space-x-12 xl:ml-14 xl:mt-[4em]
+              lg:space-x-8 lg:ml-10 lg:mt-14
+              md:space-x-5 md:ml-7 md:mt-8">
+                <div>
+                  <div class="h-12 w-12 rounded-xl justify-center place-items-center bg-orange-100 inline-flex
+                  2xl:text-4xl 2xl:w-24 2xl:h-24
+                  xl:text-3xl xl:w-20 xl:h-20
+                  lg:text-2xl lg:w-16 lg:h-16
+                  md:text-xl md:w-14 md:h-14">
+                    <Icon
+                      icon="tabler:calendar-event"
+                      color="6b21a8"
+                      className="w-6 h-6 inline-block
+                      2xl:text-4xl 2xl:w-12 2xl:h-12
+                      xl:text-3xl xl:w-10 xl:h-10
+                      lg:text-2xl lg:w-8 lg:h-8
+                      md:text-xl md:w-6 md:h-6"
+                    />
+                  </div>
+                </div>
+                <div class="justify-start">
+                  <p className="text-md text-neutral-500
+                  2xl:text-3xl
+                  xl:text-2xl
+                  lg:text-xl
+                  md:text-lg">Total Event</p>
+                  <p className="text-lg text-neutral-800 font-bold mt-1
+                  2xl:text-4xl 2xl:mt-5
+                  xl:text-3xl xl:mt-4
+                  lg:text-2xl lg:mt-3
+                  md:text-xl md:mt-2">20</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
